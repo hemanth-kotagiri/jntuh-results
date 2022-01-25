a06c781b3af2130f0a67d9a96b6d0a3a7f3e38e5
Hemanth <hemanth.kotagiri43@gmail.com>
2022-01-26 02:20:28 +0530

    Add single and multi pages

---
+++ pages/multi.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Single() {
+  return (
+    <div>
+      <Head>
+        <title>JNTUH Results Stats</title>
+      </Head>
+      <main>Get multiple Hallticket Results, page under construction.</main>
+    </div>
+  );
+}
---
+++ pages/single.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Single() {
+  return (
+    <div>
+      <Head>
+        <title>JNTUH Results Stats</title>
+      </Head>
+      <main>Get Single Hallticket Results, page under construction.</main>
+    </div>
+  );
+}
