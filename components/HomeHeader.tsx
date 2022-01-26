a4373d1e1ef1acc8b7630984cc25f04c259ba5cc
Hemanth <hemanth.kotagiri43@gmail.com>
2022-01-26 14:21:47 +0530

    Add HomeMain & HomeHeader components

---
+++ components/HomeHeader.tsx
@@ -0,0 +1,25 @@
+const HomeHeader = () => {
+  return (
+    <>
+      <h1 className="text-white text-6xl font-bold">
+        Welcome to{" "}
+        <a
+          className="text-blue-400 hover:text-blue-600 hover:underline"
+          href="https://github.com/hemanth-kotagiri/jntuh-results-stats"
+          target="_blank"
+        >
+          JNTUH Results Statistics
+        </a>
+      </h1>
+
+      <p className="text-gray-400 mt-6 text-xl">
+        Get all your results in one place.{" "}
+        <i>
+          Just your <b>hallticket</b>, to rule them all.
+        </i>
+      </p>
+    </>
+  );
+};
+
+export default HomeHeader;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import HomeMain from "../components/HomeMain";
 
 export default function Home() {
   return (
@@ -7,73 +8,7 @@ export default function Home() {
       <Head>
         <title>JNTUH Results Stats</title>
       </Head>
-
-      <main className="font-inter flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-white text-6xl font-bold">
-          Welcome to{" "}
-          <a
-            className="text-blue-400 hover:text-blue-600 hover:underline"
-            href="https://github.com/hemanth-kotagiri/jntuh-results-stats"
-            target="_blank"
-          >
-            JNTUH Results Statistics
-          </a>
-        </h1>
-
-        <p className="text-gray-400 mt-6 text-xl">
-          Get all your results in one place.{" "}
-          <i>
-            Just your <b>hallticket</b>, to rule them all.
-          </i>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <Link href="/notifications">
-            <a className="p-6 mt-6 text-left border border-gray-600 w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-blue-900 duration-300">
-              <h3 className="text-white text-2xl font-bold">
-                Notifications &rarr;
-              </h3>
-              <p className="text-gray-400 mt-4 text-xl">
-                Get all the latest notifications from JNTUH.
-              </p>
-            </a>
-          </Link>
-          <Link href="/single">
-            <a className="p-6 mt-6 text-left border border-gray-600 w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-blue-900 duration-300">
-              <h3 className="text-white text-2xl font-bold">
-                Get your Result &rarr;
-              </h3>
-              <p className="text-gray-400 mt-4 text-xl">
-                Get Results of a single hallticket number.
-              </p>
-            </a>
-          </Link>
-          <Link href="/multi">
-            <a className="p-6 mt-6 text-left border border-gray-600 w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-blue-900 duration-300">
-              <h3 className="text-white text-2xl font-bold">
-                Get Multiple Results &rarr;
-              </h3>
-              <p className="text-gray-400 mt-4 text-xl">
-                Get Results of more than one hallticket number with analytics
-              </p>
-            </a>
-          </Link>
-
-          <a
-            target="_blank"
-            href="https://hemanth-kotagiri.github.io/sgpa-rest-api-docs/"
-            className="p-6 mt-6 text-left border border-gray-600 w-96 rounded-xl hover:border-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-blue-900 duration-300"
-          >
-            <h3 className="text-white text-2xl font-bold">
-              API Documentation &rarr;
-            </h3>
-            <p className="text-gray-400 mt-4 text-xl">
-              Learn about the backend that this application uses built using
-              Python and Flask.
-            </p>
-          </a>
-        </div>
-      </main>
+      <HomeMain />
 
       <footer className="flex items-center justify-center w-full h-24 mt-4 border-t border-gray-700">
         <a
