5deb162139f514d24228e714a79ebe279b42e129
Hemanth <hemanth.kotagiri43@gmail.com>
2022-01-26 14:28:16 +0530

    Add Footer Component

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center w-full h-24 mt-4 border-t border-gray-700">
+      <a
+        className="text-white flex items-center justify-center"
+        href="https://github.com/hemanth-kotagiri"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Made with ❤️ by Hemanth.
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,17 @@
 import Head from "next/head";
-import Link from "next/link";
 import HomeMain from "../components/HomeMain";
+import Footer from "../components/Footer";
 
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-800">
       <Head>
         <title>JNTUH Results Stats</title>
       </Head>
       <HomeMain />
-
-      <footer className="flex items-center justify-center w-full h-24 mt-4 border-t border-gray-700">
-        <a
-          className="text-white flex items-center justify-center"
-          href="https://github.com/hemanth-kotagiri"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Made with ❤️ by Hemanth.
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
-}
+};
+
+export default Home;
