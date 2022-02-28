10b5134f50ed296b68c6bceddb8297231e821a85
Hemanth <hemanth.kotagiri43@gmail.com>
2022-02-28 17:21:33 +0530

    Add responsive design to result page

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import PageHead from "../components/PageHeader";
 
 export interface Props {
   examCode: string;
@@ -21,9 +22,9 @@ export interface Result {
   subject_name: string;
   grade_earned: string;
   subject_credits: string;
-  internal_marks: string;
-  external_marks: string;
-  total_marks: string;
+  internal_marks?: string;
+  external_marks?: string;
+  total_marks?: string;
 }
 
 export async function getServerSideProps({ query }: any) {
@@ -61,46 +62,62 @@ export default function Result({ data }: any) {
     results = data[1];
   }
   return (
-    <div className="text-center bg-gray-800 font-inter">
-      <div className="flex flex-col items-center justify-center text-center text-white">
-        <div className="p-6">
-          <h1 className="text-xl sm:text-4xl">{studentInfo["NAME"]}</h1>
-          <h1 className="text-lg text-gray-400 sm:text-xl">
-            {studentInfo["HTNO"]}
-          </h1>
-          <hr className="w-full border-gray-700" />
+    <div className="min-h-screen overflow-hidden text-center bg-gray-800 font-inter">
+      <PageHead
+        title={studentInfo.HTNO}
+        description={"Single Hallticket Result"}
+        url={"single"}
+      />
+      <div>
+        <div className="flex flex-col items-center justify-center text-center text-white">
+          <div className="p-6">
+            <h1 className="text-xl sm:text-4xl">{studentInfo["NAME"]}</h1>
+            <h1 className="text-lg text-gray-400 sm:text-xl">
+              {studentInfo["HTNO"]}
+            </h1>
+            <hr className="w-full border-gray-700" />
+          </div>
+          {sgpaInfo ? (
+            <div>
+              <h1 className="text-xs text-gray-400 sm:text-lg">
+                {" "}
+                SGPA/Verdict
+              </h1>
+              <h1 className="text-xl sm:text-2xl">{sgpaInfo["SGPA"]}</h1>
+              <br />
+            </div>
+          ) : null}
         </div>
-        {sgpaInfo ? (
-          <div>
-            <h1 className="text-xs text-gray-400 sm:text-lg"> SGPA/Verdict</h1>
-            <h1 className="text-xl sm:text-2xl">{sgpaInfo["SGPA"]}</h1>
-            <br />
-          </div>
-        ) : null}
-      </div>
-      <div className="text-white flex flex-col items-center justify-center">
-        {results.map((item: Result) => (
-          <div className="flex flex-col">
-            <hr className="border-gray-700" />
-            <h1>{item.subject_name}</h1>
-            <div className="flex items-center justify-center">
-              <h1 className="p-3 text-gray-400">Grade</h1>
-              <h1>{item.grade_earned}</h1>
+        <div className="flex flex-col items-center justify-center text-white">
+          {results.map((item: Result, idx: number) => (
+            <div className="flex flex-col" key={idx}>
+              <hr className="border-gray-700" />
+              <h1>{item.subject_name}</h1>
+              <div className="flex items-center justify-center">
+                <h1 className="p-3 text-gray-400">Grade</h1>
+                <h1>{item.grade_earned}</h1>
+              </div>
+              {item.external_marks &&
+              item.internal_marks &&
+              item.total_marks ? (
+                <div>
+                  <div className="flex items-center justify-center">
+                    <h1 className="p-3 text-gray-400">Internal Marks</h1>
+                    <h1>{item.internal_marks}</h1>
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <h1 className="p-3 text-gray-400">External Marks</h1>
+                    <h1>{item.external_marks}</h1>
+                  </div>
+                  <div className="flex items-center justify-center">
+                    <h1 className="p-3 text-gray-400">Total Marks</h1>
+                    <h1>{item.total_marks}</h1>
+                  </div>
+                </div>
+              ) : null}
             </div>
-            <div className="flex items-center justify-center">
-              <h1 className="p-3 text-gray-400">Internal Marks</h1>
-              <h1>{item.internal_marks}</h1>
-            </div>
-            <div className="flex items-center justify-center">
-              <h1 className="p-3 text-gray-400">External Marks</h1>
-              <h1>{item.external_marks}</h1>
-            </div>
-            <div className="flex items-center justify-center">
-              <h1 className="p-3 text-gray-400">Total Marks</h1>
-              <h1>{item.total_marks}</h1>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
