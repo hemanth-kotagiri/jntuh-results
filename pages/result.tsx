3b6b3f85523f52d7fa4fac00205e42c8b3cc770c
Hemanth <hemanth.kotagiri43@gmail.com>
2022-03-03 22:36:45 +0530

    Add colors

--- pages/result.tsx
+++ pages/result.tsx
@@ -90,40 +90,60 @@ export default function Result({ data }: any) {
                 {" "}
                 SGPA/Verdict
               </h1>
-              <h1 className="text-xl sm:text-2xl">{sgpaInfo["SGPA"]}</h1>
+              <h1
+                className={`text-xl sm:text-2xl ${
+                  sgpaInfo.SGPA !== "FAIL" ? "text-green-400" : "text-red-400"
+                }`}
+              >
+                {sgpaInfo["SGPA"]}
+              </h1>
               <br />
             </div>
           ) : null}
         </div>
-        <div className="flex flex-col items-center justify-center text-white">
-          {results.map((item: Result, idx: number) => (
-            <div className="flex flex-col" key={idx}>
-              <hr className="border-gray-700" />
-              <h1>{item.subject_name}</h1>
-              <div className="flex items-center justify-center">
-                <h1 className="p-3 text-gray-400">Grade</h1>
-                <h1>{item.grade_earned}</h1>
-              </div>
-              {item.external_marks &&
-              item.internal_marks &&
-              item.total_marks ? (
-                <div>
-                  <div className="flex items-center justify-center">
-                    <h1 className="p-3 text-gray-400">Internal Marks</h1>
-                    <h1>{item.internal_marks}</h1>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <h1 className="p-3 text-gray-400">External Marks</h1>
-                    <h1>{item.external_marks}</h1>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <h1 className="p-3 text-gray-400">Total Marks</h1>
-                    <h1>{item.total_marks}</h1>
-                  </div>
+        <div className="flex flex-col items-center">
+          <div className="text-white">
+            {results.map((item: Result, idx: number) => (
+              <div>
+                <hr className="border-gray-700" />
+                <div className="flex flex-row justify-between" key={idx}>
+                  <h1 className="p-2">{item.subject_name}</h1>
+                  <h1
+                    className={`p-2 ${
+                      item.grade_earned === "F"
+                        ? "text-red-400"
+                        : "text-green-400"
+                    }`}
+                  >
+                    {item.grade_earned}
+                  </h1>
                 </div>
-              ) : null}
-            </div>
-          ))}
+                {item.external_marks &&
+                item.internal_marks &&
+                item.total_marks ? (
+                  <div className="flex flex-row place-items-start">
+                    <div className="flex flex-col xm:flex-row items-center justify-center">
+                      <h1 className="p-3 text-gray-400 text-sm">
+                        Internal Marks
+                      </h1>
+                      <h1 className="text-sm">{item.internal_marks}</h1>
+                    </div>
+                    <div className="flex flex-col xm:flex-row items-center justify-center">
+                      <h1 className="p-3 text-gray-400 text-sm">
+                        External Marks
+                      </h1>
+                      <h1 className="text-sm">{item.external_marks}</h1>
+                    </div>
+                    <div className="flex flex-col xm:flex-row items-center justify-center">
+                      <h1 className="p-3 text-gray-400 text-sm">Total Marks</h1>
+                      <h1 className="text-sm">{item.total_marks}</h1>
+                    </div>
+                  </div>
+                ) : null}
+                <hr className="border-gray-700" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
     fontFamily: {
       inter: ["Inter", "sans-serif"],
     },
+    screens: {
+      //custom
+      xm: "350px",
+      // default
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
