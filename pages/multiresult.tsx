8c67642f12a36cb5895df17b7ce82828d98b23dd
Hemanth <hemanth.kotagiri43@gmail.com>
2022-03-10 01:48:31 +0530

    Add reloading timeout function for multipage to avoid serverless timeout

--- pages/multiresult.tsx
+++ pages/multiresult.tsx
@@ -3,6 +3,7 @@ import PageHead from '../components/PageHeader'
 import SubjectDetails from '../components/definedTypes'
 import { ResultSubjectsList } from '../components/ResultSubjectsList'
 import { StudentInfoAndGPA } from '../components/StudentInfoAndGPA'
+import { useEffect } from 'react'
 
 interface queryProps {
   examCode: string
@@ -27,11 +28,6 @@ interface studentInfoProps {
   'COLLEGE CODE': string
 }
 
-
-function timeout(delay: number) {
-    return new Promise( res => setTimeout(res, delay) );
-}
-
 export async function getServerSideProps(givenData: Props) {
   const query = givenData.query
   var url = 'https://results-restapi.herokuapp.com/api/bulk/calculate'
@@ -45,11 +41,6 @@ export async function getServerSideProps(givenData: Props) {
   url += '&degree=' + query.degree
   const resp = await axios.get(url)
   let data = await resp.data
-  while (data['result'] === 'loading') {
-    const resp = await axios.get(url)
-    data = await resp.data
-    await timeout(5000);
-  }
   return {
     props: {
       data: data,
@@ -57,23 +48,44 @@ export async function getServerSideProps(givenData: Props) {
   }
 }
 
+function refreshPage() {
+  window.location.reload()
+}
+
 export default function MultiResult({ data }: any) {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      refreshPage()
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className='min-h-screen overflow-hidden text-center bg-gray-800 font-inter'>
-      {data.map((item: any, idx: number) => (
-        <div key={idx}>
-          {item.length > 1 ? (
-            <div>
-              <StudentInfoAndGPA
-                studentName={item[1].NAME}
-                studentHTNO={item[1].HTNO}
-                sgpaInfo={item[0]}
-              />
-              <ResultSubjectsList results={item[2]} />
-            </div>
-          ) : null}
+      {data['result'] === 'loading' ? (
+        <div>
+          <h1 className='text-xl sm:text-4xl m-6 p-6 text-white text-center'>
+            Loading the results, please wait...
+          </h1>
         </div>
-      ))}
+      ) : (
+        <div className='min-h-screen overflow-hidden text-center bg-gray-800 font-inter'>
+          {data.map((item: any, idx: number) => (
+            <div key={idx}>
+              {item.length > 1 ? (
+                <div>
+                  <StudentInfoAndGPA
+                    studentName={item[1].NAME}
+                    studentHTNO={item[1].HTNO}
+                    sgpaInfo={item[0]}
+                  />
+                  <ResultSubjectsList results={item[2]} />
+                </div>
+              ) : null}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
