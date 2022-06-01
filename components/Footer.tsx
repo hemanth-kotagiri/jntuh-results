573fac144ac0c49df102228708a9989d869ae3c7
Hemanth <hemanth.kotagiri43@gmail.com>
2022-06-01 14:00:10 +0530

    Add: font-inter to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <footer className='flex items-center justify-center w-full h-24 mt-4 border-t border-gray-700'>
+    <footer className='flex items-center justify-center w-full h-24 mt-4 border-t border-gray-800 font-inter'>
       <a
         className='text-white flex items-center justify-center'
         href='https://github.com/hemanth-kotagiri'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Made with ❤️ by Hemanth.
+        Made with ❤️ by Hemanth
       </a>
     </footer>
   )
--- components/HomeHeader.tsx
+++ components/HomeHeader.tsx
@@ -1,25 +1,25 @@
 const HomeHeader = () => {
   return (
     <>
-      <h1 className="text-white text-3xl font-bold sm:text-6xl">
-        Welcome to{" "}
+      <h1 className='text-white text-3xl font-bold sm:text-6xl'>
+        Welcome to{' '}
         <a
-          className="text-blue-400 hover:text-blue-600 hover:underline"
-          href="https://github.com/hemanth-kotagiri/jntuh-results-stats"
-          target="_blank"
+          className='text-blue-400 hover:text-blue-600 hover:underline'
+          href='https://github.com/hemanth-kotagiri/jntuh-results-stats'
+          target='_blank'
         >
           JNTUH Results Statistics
         </a>
       </h1>
 
-      <p className="text-gray-400 mt-6 text-base sm:text-xl">
-        Get all your results in one place.{" "}
+      <p className='text-gray-400 mt-6 text-base sm:text-xl'>
+        Get all your results in one place.{' '}
         <i>
           Just your <b>hallticket</b>, to rule them all.
         </i>
       </p>
     </>
-  );
-};
+  )
+}
 
-export default HomeHeader;
+export default HomeHeader
