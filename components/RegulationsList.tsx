eb28c6712bab37e799d0a41879cdd288c3e2ad77
Hemanth <hemanth.kotagiri43@gmail.com>
2022-09-14 01:29:21 +0530

    Fix: Formatting files

--- components/RegulationsList.tsx
+++ components/RegulationsList.tsx
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react'
 
 export interface Props {
-  regulations: string[];
-  handleClick: Function;
+  regulations: string[]
+  handleClick: Function
 }
 
 export const RegulationsList = (props: Props) => {
   return (
     <div>
-      <div className="flex flex-col m-2 sm:flex-row">
+      <div className='flex flex-col m-2 sm:flex-row'>
         {props.regulations.map((item: string, idx: number) => (
-          <div className="m-2" key={idx}>
+          <div className='m-2' key={idx}>
             <input
-              type="radio"
+              type='radio'
               value={item}
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              name="regulation"
+              className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+              name='regulation'
               id={idx.toString()}
               onClick={(e: React.FormEvent<HTMLInputElement>) =>
                 props.handleClick(e)
               }
             />
             <label
-              className="ml-2 text-lg dark:text-white sm:text-2xl cursor-pointer"
+              className='ml-2 text-lg dark:text-white sm:text-2xl cursor-pointer'
               htmlFor={idx.toString()}
             >
               {item}
@@ -31,5 +31,5 @@ export const RegulationsList = (props: Props) => {
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
